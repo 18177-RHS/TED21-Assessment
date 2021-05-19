4f7b0c8bca46a71efb993d5fb9fdc63232b7b259
18177-RHS <82122132+18177-RHS@users.noreply.github.com>
2021-05-20 09:10:23 +1200

    Add files via upload
    Add nuclear question where there is a 20% chance it will explode

---
+++ assmet V4.py
@@ -0,0 +1,67 @@
+#imports
+import random
+
+
+
+#Varbiles
+
+budget = 300
+progress = 0
+question_db = [
+    ["Should we start capturing Co2?",["Yes   -100B","No    +100B"], [-100, 100]],
+    ["Which renewable energy(s) should we switch to?",["Wind   -50B", "Solar  -100B", "Hydro  -200B"],[-50, -100, -200]],
+        ["Nuclear energy?",["Yes     -100B","No"],[-100, 100]],
+    ["What percent should we still use Fossil fuels?",[""],[-100, 100]]
+    ]
+
+#funtions
+def display2(progress, budget):
+    print(question_db[progress][0] + "        Budget: %d" %(budget)+"B")
+    print("A: "+question_db[progress][1][0])
+    print("B: "+question_db[progress][1][1])
+    return check(input(">>> "),progress, budget)
+
+
+
+def display3(progress, budget):
+    print(question_db[progress][0] + "        Budget: %d" %(budget)+"B")
+    print("A: "+question_db[progress][1][0])
+    print("B: "+question_db[progress][1][1])
+    print("C: "+question_db[progress][1][2])
+    return check(input(">>> "),progress, budget)    
+
+def displayChance(progress, budget):
+    print(question_db[progress][0] + "        Budget: %d" %(budget)+"B")
+    print("" +question_db[progress][1][0])
+    chance = random.randint(0, 10)
+    if chance < 2:
+        print("Boom")
+        progress = 100
+    else:
+        return budget  
+    
+
+
+def check(user_answer, progress, budget):
+    if user_answer.upper() == "A":
+        budget += question_db[progress][2][0]
+    if user_answer.upper() == "B":
+        budget += question_db[progress][2][1]
+    if user_answer.upper() == "C":
+        budget += question_db[progress][2][2]
+    return budget
+
+
+
+
+while progress < len(question_db):
+    if len(question_db[progress][1]) == 2:
+        budget = display2(progress, budget)
+        progress += 1
+    elif len(question_db[progress][1]) == 3:
+        budget = display3(progress, budget)
+        progress += 1
+    elif len(question_db[progress][1]) == 1:
+        budget = displayChance(progress, budget)
+        progress += 1        
+    
